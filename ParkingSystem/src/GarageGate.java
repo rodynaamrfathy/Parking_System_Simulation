f3065256781dc63608a56352f963b17bc84df46a
rodynaamrfathy <rodynaamr@icloud.com>
2024-11-09 18:37:03 +0200

    classes defined

--- ParkingSystem/src/GarageGate.java
+++ ParkingSystem/src/GarageGate.java
@@ -1,4 +1,7 @@
 public class GarageGate extends Thread{
+
+    private static final Semaphore garageSemaphore = new Semaphore();// hwa ehh el max amount tl kol gate mmken t5do?
+
     public void run()
     {
         
